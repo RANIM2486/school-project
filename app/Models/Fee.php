a0eaf7e668545990a2773af4e3b84802a0536a4c
Ranim <ranimhj@email.com>
2025-06-21 19:21:30 +0300

    fee

--- app/Models/Fee.php
+++ app/Models/Fee.php
@@ -8,9 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Fee extends Model
 {
     protected $fillable=['student_id' ,'amount','type','status','due_date'];
-    public function student()
-{
-    return $this->belongsTo(Student::class);
-}
 
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+    public function installments()
+{
+    return $this->hasMany(Installment::class);
+}
 }
