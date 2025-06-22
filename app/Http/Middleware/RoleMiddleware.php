5db29a59092a99502b2a1e49a97a6edd89929b83
ranimkh2004 <ranimkh2025@gmail.com>
2025-06-22 15:33:01 +0300

    updating middelware and ken

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Middleware;
 
+
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 //use illuminate\Support\Facades
+use Illuminate\Support\Facades\Log;
+
 
 class RoleMiddleware
 {
@@ -15,22 +18,30 @@ class RoleMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next,$roles)
-    {
+  public function handle(Request $request, Closure $next, $roles)
+{
+    $user = $request->user();
 
-      $user=$request->user();
-        if (!$user) {
-            return response()->json(['message' => 'Unauthorized'], 401);
-        }
+    if (!$user) {
+        return response()->json(['message' => 'Unauthorized'], 401);
+    }
 
-        foreach($roles as $role){
-            if($user->role===$role)
-         {
+    // إضافة ديباغ
+    log::info('User role:', ['role' => $user->role]);
+
+    $userRoles = array_map('trim', explode(',', strtolower($user->role)));
+    $requiredRoles = array_map('trim', explode(',', strtolower($roles)));
+
+    foreach ($requiredRoles as $role) {
+        if (in_array($role, $userRoles)) {
             return $next($request);
-         }
-            }
-        return response()->json(['message' => 'Forbidden'], 403);
         }
-     }
+    }
+
+    return response()->json(['message' => 'Forbidden'], 403);
+}
+
+
+}
 
 
--- app/Http/kernel.php
+++ app/Http/kernel.php
@@ -8,7 +8,7 @@ class Kernel extends HttpKernel
 {
     // Middleware العامة (لكل الطلبات، غالباً مو ضرورية كلها حالياً، بس نخلي المهم منها)
     protected $middleware = [
-        'role'=>\App\Http\Middleware\RoleMiddleware::class,
+
         \Illuminate\Http\Middleware\HandleCors::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
@@ -18,6 +18,7 @@ class Kernel extends HttpKernel
     protected $middlewareGroups = [
         'api' => [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+             'auth:sanctum',
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
